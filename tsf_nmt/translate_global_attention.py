1b8cf4859ec6e5d10ae3568916c4cf49dcf85299
gian <gian.salton@gmail.com>
2016-02-10 11:54:21 +0000

    Corrections on the validation step when training.

--- tsf_nmt/translate_global_attention.py
+++ tsf_nmt/translate_global_attention.py
@@ -35,72 +35,67 @@ import attention
 from train_ops import train_nmt
 from translate_ops import decode_from_stdin, decode_from_file
 
+flags = tf.flags
 
 # flags related to the model optimization
-tf.app.flags.DEFINE_float('learning_rate', 0.001, 'Learning rate.')
-tf.app.flags.DEFINE_float('learning_rate_decay_factor', 1.0, 'Learning rate decays by this much. Setting it to 1.0 will not affect the learning rate.')
-tf.app.flags.DEFINE_integer('start_decay', 0, 'Start learning rate decay at this epoch. Set to 0 to use patience.')
-tf.app.flags.DEFINE_string('optimizer', 'adam',
+flags.DEFINE_float('learning_rate', 0.001, 'Learning rate.')
+flags.DEFINE_float('learning_rate_decay_factor', 1.0, 'Learning rate decays by this much. Setting it to 1.0 will not affect the learning rate.')
+flags.DEFINE_integer('start_decay', 0, 'Start learning rate decay at this epoch. Set to 0 to use patience.')
+flags.DEFINE_string('optimizer', 'adam',
                            'Name of the optimizer to use (adagrad, adam, rmsprop or sgd')
 
-tf.app.flags.DEFINE_float('max_gradient_norm', 5.0, 'Clip gradients to this norm.')
-tf.app.flags.DEFINE_integer('batch_size', 32, 'Batch size to use during training.')
-tf.app.flags.DEFINE_integer('beam_size', 12, 'Max size of the beam used for decoding.')
-tf.app.flags.DEFINE_integer('max_len', 100, 'Max size of the beam used for decoding.')
-tf.app.flags.DEFINE_integer('max_epochs', 12,  'Max number of epochs to use during training. The actual value will be (max_epochs-1) as it is 0-based.')
-tf.app.flags.DEFINE_integer('max_train_data_size', 0,
-                            'Limit on the size of training data (0: no limit).')
+flags.DEFINE_float('max_gradient_norm', 5.0, 'Clip gradients to this norm.')
+flags.DEFINE_integer('batch_size', 32, 'Batch size to use during training.')
+flags.DEFINE_integer('beam_size', 12, 'Max size of the beam used for decoding.')
+flags.DEFINE_integer('max_len', 100, 'Max size of the beam used for decoding.')
+flags.DEFINE_integer('max_epochs', 12,  'Max number of epochs to use during training. The actual value will be (max_epochs-1) as it is 0-based.')
+flags.DEFINE_integer('max_train_data_size', 0, 'Limit on the size of training data (0: no limit).')
 
 # flags related to model architecture
-tf.app.flags.DEFINE_string('model', 'seq2seq', 'one of these models: seq2seq')
-tf.app.flags.DEFINE_string('attention_type', 'global', 'Which type of attention to use. One of local, global and hybrid.')
-tf.app.flags.DEFINE_string('content_function', attention.VINYALS_KAISER, 'Type of content-based function to define the attention. One of vinyals_kayser, luong_general and luong_dot')
-tf.app.flags.DEFINE_boolean('use_lstm', True, 'Whether to use LSTM units. Default to False.')
-tf.app.flags.DEFINE_boolean('input_feeding', False, 'Whether to input the attention states as part of input to the decoder at each timestep. Default to False.')
-tf.app.flags.DEFINE_boolean('output_attention', True, 'Whether to pay attention on the decoder outputs. Default to False.')
-tf.app.flags.DEFINE_integer('proj_size', 300, 'Size of words projection.')
-tf.app.flags.DEFINE_integer('hidden_size', 300, 'Size of each layer.')
-tf.app.flags.DEFINE_integer('num_layers', 2, 'Number of layers in each component of the model.')
+flags.DEFINE_string('model', 'seq2seq', 'one of these models: seq2seq')
+flags.DEFINE_string('attention_type', 'global', 'Which type of attention to use. One of local, global and hybrid.')
+flags.DEFINE_string('content_function', attention.VINYALS_KAISER, 'Type of content-based function to define the attention. One of vinyals_kayser, luong_general and luong_dot')
+flags.DEFINE_boolean('use_lstm', True, 'Whether to use LSTM units. Default to False.')
+flags.DEFINE_boolean('input_feeding', False, 'Whether to input the attention states as part of input to the decoder at each timestep. Default to False.')
+flags.DEFINE_boolean('output_attention', True, 'Whether to pay attention on the decoder outputs. Default to False.')
+flags.DEFINE_integer('proj_size', 300, 'Size of words projection.')
+flags.DEFINE_integer('hidden_size', 300, 'Size of each layer.')
+flags.DEFINE_integer('num_layers', 2, 'Number of layers in each component of the model.')
 
-tf.app.flags.DEFINE_float('dropout', 0.0, 'Dropout rate. When the value is 0.0 dropout is turned off. Optimal should be 0.2 as indicated by Zaremba et al. (2014)')
+flags.DEFINE_float('dropout', 0.0, 'Dropout rate. When the value is 0.0 dropout is turned off. Optimal should be 0.2 as indicated by Zaremba et al. (2014)')
 
 # flags related to the source and target vocabularies
-tf.app.flags.DEFINE_integer('src_vocab_size', 30000, 'Source language vocabulary size.')
-tf.app.flags.DEFINE_integer('tgt_vocab_size', 30000, 'Target vocabulary size.')
+flags.DEFINE_integer('src_vocab_size', 30000, 'Source language vocabulary size.')
+flags.DEFINE_integer('tgt_vocab_size', 30000, 'Target vocabulary size.')
 
 # information about the datasets and their location
-tf.app.flags.DEFINE_string('model_name', 'model_lstm_global_output_vinyals_hid300_proj300_en30000_pt30000_adam.ckpt',
+flags.DEFINE_string('model_name', 'model_lstm_global_output_vinyals_hid300_proj300_en30000_pt30000_adam.ckpt',
                            'Model name')
-tf.app.flags.DEFINE_string('data_dir', '/home/gian/data/', 'Data directory')
-tf.app.flags.DEFINE_string('train_dir', '/home/gian/train_global/model_lstm_global_output_vinyals_hid300_proj300_en30000_pt30000_adam/', 'Train directory')
-tf.app.flags.DEFINE_string('best_models_dir', '/home/gian/train_global/', 'Train directory')
-# tf.app.flags.DEFINE_string('train_data', 'train.tok.%s', 'Data for training.')
-# tf.app.flags.DEFINE_string('valid_data', 'newstest2013.tok.%s', 'Data for validation.')
-# tf.app.flags.DEFINE_string('test_data', 'newstest2013.tok.%s', 'Data for testing.')
-tf.app.flags.DEFINE_string('train_data', 'fapesp-v2.pt-en.train.tok.%s', 'Data for training.')
-tf.app.flags.DEFINE_string('valid_data', 'fapesp-v2.pt-en.dev.tok.%s', 'Data for validation.')
-tf.app.flags.DEFINE_string('test_data', 'fapesp-v2.pt-en.test-a.tok.%s', 'Data for testing.')
-tf.app.flags.DEFINE_string('vocab_data', '', 'Training directory.')
-# tf.app.flags.DEFINE_string('source_lang', 'de', 'Source language extension.')
-# tf.app.flags.DEFINE_string('target_lang', 'en', 'Target language extension.')
-tf.app.flags.DEFINE_string('source_lang', 'en', 'Source language extension.')
-tf.app.flags.DEFINE_string('target_lang', 'pt', 'Target language extension.')
+flags.DEFINE_string('data_dir', '/home/gian/data/', 'Data directory')
+flags.DEFINE_string('train_dir', '/home/gian/train_global/model_lstm_global_output_vinyals_hid300_proj300_en30000_pt30000_adam/', 'Train directory')
+flags.DEFINE_string('best_models_dir', '/home/gian/train_global/', 'Train directory')
+flags.DEFINE_string('train_data', 'fapesp-v2.pt-en.train.tok.%s', 'Data for training.')
+flags.DEFINE_string('valid_data', 'fapesp-v2.pt-en.dev.tok.%s', 'Data for validation.')
+flags.DEFINE_string('test_data', 'fapesp-v2.pt-en.test-a.tok.%s', 'Data for testing.')
+flags.DEFINE_string('vocab_data', '', 'Training directory.')
+flags.DEFINE_string('source_lang', 'en', 'Source language extension.')
+flags.DEFINE_string('target_lang', 'pt', 'Target language extension.')
 
 # verbosity and checkpoints
-tf.app.flags.DEFINE_integer('steps_per_checkpoint', 100, 'How many training steps to do per checkpoint.')
-tf.app.flags.DEFINE_integer('steps_per_validation', 1000, 'How many training steps to do between each validation.')
-tf.app.flags.DEFINE_integer('steps_verbosity', 10, 'How many training steps to do between each information print.')
+flags.DEFINE_integer('steps_per_checkpoint', 100, 'How many training steps to do per checkpoint.')
+flags.DEFINE_integer('steps_per_validation', 1000, 'How many training steps to do between each validation.')
+flags.DEFINE_integer('steps_verbosity', 10, 'How many training steps to do between each information print.')
 
 # pacience flags (learning_rate decay and early stop)
-tf.app.flags.DEFINE_integer('lr_rate_patience', 3, 'How many training steps to monitor.')
-tf.app.flags.DEFINE_integer('early_stop_patience', 20, 'How many training steps to monitor.')
-tf.app.flags.DEFINE_integer('early_stop_after_epoch', 11, 'Start monitoring early_stop after this epoch.')
+flags.DEFINE_integer('lr_rate_patience', 3, 'How many training steps to monitor.')
+flags.DEFINE_integer('early_stop_patience', 20, 'How many training steps to monitor.')
+flags.DEFINE_integer('early_stop_after_epoch', 11, 'Start monitoring early_stop after this epoch.')
 
 # decoding/testing flags
-tf.app.flags.DEFINE_boolean('decode_file', False, 'Set to True for decoding sentences in a file.')
-tf.app.flags.DEFINE_boolean('decode_input', False, 'Set to True for interactive decoding.')
+flags.DEFINE_boolean('decode_file', False, 'Set to True for decoding sentences in a file.')
+flags.DEFINE_boolean('decode_input', False, 'Set to True for interactive decoding.')
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = flags.FLAGS
 
 # We use a number of buckets and pad to the closest one for efficiency.
 # See seq2seq_model.Seq2SeqModel for details of how they work.
